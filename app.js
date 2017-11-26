207add450327ffa57d4cb7822f9b55db7161c19b
Diego <diego_henriquerodrigues@hotmail.com>
2017-11-26 10:39:09 -0200

    Add mongodb

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+let mongoose = require('./src/db/connection')
 
 var index = require('./routes/index');
 var users = require('./routes/users');
